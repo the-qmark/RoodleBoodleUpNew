a4a303d46089c51d74f2ed1808f142c17ef6529a
the-qmark <egormalyukow@gmail.com>
2020-04-27 16:41:03 +0400

    Изменил спавн (да опять и что и что)

--- Assets/_SCRIPTS/GameManager/MainSpawner.cs
+++ Assets/_SCRIPTS/GameManager/MainSpawner.cs
@@ -7,31 +7,80 @@ public class MainSpawner : MonoBehaviour
     [SerializeField] private Coin _coinPref;
     [SerializeField] private BubblePickUp _bubbleMovePref;
 
+    private GameObject _bubble;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        _bubble = Instantiate(_bubbleMovePref.gameObject, transform.position, Quaternion.identity);
+        _bubble.SetActive(false);
+    }
 
     private int GetChance()
     {
-        return Random.Range(0, 20);
+        int r = Random.Range(0, 20);
+        Debug.Log("random = " + r);
+        return r;
     }
 
 
-    public void SpawnCoin()
+    public void Spawn()
     {
-        if (GetChance() > 8)
+        int _chance = GetChance();
+
+        if (_chance > 17)
         {
-            GameObject coin = Instantiate(_coinPref.gameObject, transform.position, Quaternion.identity);
-            Destroy(coin, 30);
+            SpawnBubble();
+        }
+        else 
+        if (_chance > 8)
+        {
+            SpawnCoin();
         }
     }
 
 
-    public void SpawnBubble()
+    private void SpawnCoin()
     {
-        if (GetChance() > 15)
-        {
-            GameObject bubble = Instantiate(_bubbleMovePref.gameObject, transform.position, Quaternion.identity);
-            Destroy(bubble, 30);
-        }
+        //if (GetChance() > 8)
+        //{
+        //    GameObject coin = Instantiate(_coinPref.gameObject, transform.position, Quaternion.identity);
+        //    Destroy(coin, 30);
+        //}
+
+        GameObject coin = Instantiate(_coinPref.gameObject, transform.position, Quaternion.identity);
+        Destroy(coin, 30);
     }
 
-    
+
+    private void SpawnBubble()
+    {
+        //if (GetChance() > 15)
+        //{
+        //    GameObject bubble = Instantiate(_bubbleMovePref.gameObject, transform.position, Quaternion.identity);
+        //    Destroy(bubble, 30);
+        //}
+
+        Vector3 _point = _camera.WorldToViewportPoint(_bubble.transform.position);
+        Debug.Log("point = " + _point.y);
+
+        if (_point.y < 0)
+        {
+            _bubble.SetActive(false);
+        }
+
+        if (_bubble.activeSelf)
+        {
+            return;
+        }
+        else
+        {
+            _bubble.transform.position = transform.position;
+            _bubble.SetActive(true);
+        }
+
+        //GameObject bubble = Instantiate(_bubbleMovePref.gameObject, transform.position, Quaternion.identity);
+        //Destroy(bubble, 30);
+    }
 }
--- Assets/_SCRIPTS/GameManager/ScoreCounter.cs
+++ Assets/_SCRIPTS/GameManager/ScoreCounter.cs
@@ -9,20 +9,21 @@ public class ScoreCounter : MonoBehaviour
     [SerializeField] private Transform _camera;
     [SerializeField] private TMP_Text _currretnScoreText;
     [Space]
-    [SerializeField] private int _newStageLimit;
-    [SerializeField] private int _coinLimit;
-    [SerializeField] private int _bubbleLimit;
-    [SerializeField] private int _letterLimit;
+    [SerializeField] private int _spawnLimit;
+    //[SerializeField] private int _newStageLimit;
+    //[SerializeField] private int _coinLimit;
+    //[SerializeField] private int _bubbleLimit;
+    //[SerializeField] private int _letterLimit;
 
-    private int _newStageLimitIncrease;
-    private int _coinLimitIncrease;
-    private int _bubbleLimitIncrease;
-    private int _letterLimitIncrease;
+    private int _spawnLimitIncrease;
+    //private int _coinLimitIncrease;
+    //private int _bubbleLimitIncrease;
+    //private int _letterLimitIncrease;
     
-    public UnityEvent OnNewStageLimit;
-    public UnityEvent OnCoinLimit;
-    public UnityEvent OnBubbleLimit;
-    public UnityEvent OnLetterLimit;
+    public UnityEvent SpawnLimit;
+    //public UnityEvent OnCoinLimit;
+    //public UnityEvent OnBubbleLimit;
+    //public UnityEvent OnLetterLimit;
 
     
     private float _currentScore;
@@ -33,10 +34,10 @@ public class ScoreCounter : MonoBehaviour
         _currentScore = 0;
         _currretnScoreText.text = _currentScore.ToString("000000");
 
-        _newStageLimitIncrease = _newStageLimit;
-        _coinLimitIncrease = _coinLimit;
-        _bubbleLimitIncrease = _bubbleLimit*3;
-        _letterLimitIncrease = _letterLimit;
+        _spawnLimitIncrease = _spawnLimit;
+        //_coinLimitIncrease = _coinLimit;
+        //_bubbleLimitIncrease = _bubbleLimit*3;
+        //_letterLimitIncrease = _letterLimit;
     }
 
     void Update()
@@ -44,30 +45,30 @@ public class ScoreCounter : MonoBehaviour
         _currentScore = _camera.position.y * 0.2f;
         _currretnScoreText.text = _currentScore.ToString("000000");
 
-        if ((int)_currentScore >= _newStageLimit)
+        if ((int)_currentScore >= _spawnLimit)
         {
-            OnNewStageLimit?.Invoke();
-            _newStageLimit += _newStageLimitIncrease;
+            SpawnLimit?.Invoke();
+            _spawnLimit += _spawnLimitIncrease;
         }
 
-        if ((int)_currentScore >= _coinLimit)
-        {
-            OnCoinLimit?.Invoke();
-            _coinLimit += _coinLimitIncrease;
-            //Debug.Log("OnCoin");
-        }
+        //if ((int)_currentScore >= _coinLimit)
+        //{
+        //    OnCoinLimit?.Invoke();
+        //    _coinLimit += _coinLimitIncrease;
+        //    //Debug.Log("OnCoin");
+        //}
 
-        if ((int)_currentScore >= _bubbleLimit)
-        {
-            OnBubbleLimit?.Invoke();
-            _bubbleLimit += _bubbleLimitIncrease;
-        }
+        //if ((int)_currentScore >= _bubbleLimit)
+        //{
+        //    OnBubbleLimit?.Invoke();
+        //    _bubbleLimit += _bubbleLimitIncrease;
+        //}
 
-        if ((int)_currentScore >= _letterLimit)
-        {
-            OnLetterLimit?.Invoke();
-            _letterLimit += _letterLimitIncrease;
-        }
+        //if ((int)_currentScore >= _letterLimit)
+        //{
+        //    OnLetterLimit?.Invoke();
+        //    _letterLimit += _letterLimitIncrease;
+        //}
     }
 
     public void OnGameOver()
